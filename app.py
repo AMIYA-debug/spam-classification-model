176ccd8f57151514b6337c5c3a0fa26e7d4d65ae
AMIYA-debug <amiyabehera0990@gmail.com>
2025-10-01 01:07:42 +0530

    new file

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ import pickle
 
 app = Flask(__name__)
 
-# Load BoW model + vectorizer
-bow_model = pickle.load(open("bow_model_1.pkl", "rb"))
-bow_vectorizer = pickle.load(open("bow_vectorizer_1.pkl", "rb"))
 
-# Load TF-IDF model + vectorizer
-tfidf_model = pickle.load(open("tfidf_model_2.pkl", "rb"))
-tfidf_vectorizer = pickle.load(open("tfidf_vectorizer_2.pkl", "rb"))
+bow_model = pickle.load(open("model/bow_model_1.pkl", "rb"))
+bow_vectorizer = pickle.load(open("model/bow_vectorizer_1.pkl", "rb"))
 
-# Example accuracies from training (update with real scores)
+
+tfidf_model = pickle.load(open("model/tfidf_model_2.pkl", "rb"))
+tfidf_vectorizer = pickle.load(open("model/tfidf_vectorizer_2.pkl", "rb"))
+
+
 bow_accuracy = 97.8
 tfidf_accuracy = 98.2
 
@@ -24,17 +24,17 @@ def predict():
     if request.method == 'POST':
         message = request.form['message']
 
-        # --- BoW Prediction ---
+        
         bow_vector = bow_vectorizer.transform([message])
         bow_pred = bow_model.predict(bow_vector)[0]  # 0 or 1
         bow_label = "Not Spam" if bow_pred == 1 else "Spam"
 
-        # --- TF-IDF Prediction ---
+        
         tfidf_vector = tfidf_vectorizer.transform([message])
         tfidf_pred = tfidf_model.predict(tfidf_vector)[0]  # 0 or 1
         tfidf_label = "Not Spam" if tfidf_pred == 1 else "Spam"
 
-        # Decide which model is better (based on accuracy)
+        
         better_model = "TF-IDF" if tfidf_accuracy > bow_accuracy else "BoW"
 
         return render_template("index.html",
